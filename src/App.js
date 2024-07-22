778aee64f359542895968f547036af026c1d1065
Vitória Santana <vitoriagcsantana@gmail.com>
2024-07-22 14:07:59 -0300

    uso do useState com listas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Nome from "./components/Nome";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+   'Pagar a conta de luz', 
+   'Estudar React JS'
+  ]);
+
+  const [user, setUser] = useState({});
+
+  function handleRegister(e){
+    e.preventDefault();
+
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }
+
+  return(
+    <div>
+      <h1>Cadastrando usuário</h1>
+
+      <form onSubmit={handleRegister}>
+        <label>Nome da tarefa: </label>
+        <br/>
+        <input placeholder="Digite uma tarefa"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}></input>
+
+        <br/><br/>
+        <button type="submit">Registar</button>
+      </form>
+
+      <br/><br/>
+
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
     </div>
   );
 }
