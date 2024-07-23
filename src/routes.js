9718e102e28f97e74041c71dc3978165220bd66b
Vitória Santana <vitoriagcsantana@gmail.com>
2024-07-23 15:37:14 -0300

    Routes pt.2

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
 import Contato from './pages/Contato'
+import Erro from './pages/Erro';
+import Produto from './pages/Produto';
 
 import Header from './components/Header';
 
@@ -13,6 +15,12 @@ const AppRoutes = () => {
             <Route path="/" element={<Home />} />
             <Route path="/sobre" element={<Sobre />} />
             <Route path="/contato" element={<Contato />} />
+
+            {/* Rota para renderizar página com id*/}  
+            <Route path="/produto/:id" element={<Produto />} />
+
+            {/* Rota para renderizar página de erro, quando uma página não for encontrada*/}  
+            <Route path="*" element={<Erro />} />
             
             {/* Rota para renderizar mais de uma página ao mesmo tempo */}  
             <Route path="/both" element={<>
