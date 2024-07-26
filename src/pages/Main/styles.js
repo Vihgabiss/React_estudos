3a0d999564ac7fef2ec1670884b965b8f6728f75
Vitória Santana <vitoriagcsantana@gmail.com>
2024-07-26 16:12:22 -0300

    botão de adicionar com animação

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 
 export const Container = styled.div`
   max-width: 700px;
@@ -21,29 +21,53 @@ export const Container = styled.div`
 `;
 
 export const Form = styled.form`
-margin-top: 30px;
-display: flex;
-flex-direction: row;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
 
-input{
+  input {
     flex: 1;
-    border: 1px solid #DDD;
+    border: 1px solid #ddd;
     padding: 10px 15px;
     border-radius: 4px;
     font-size: 17px;
-}
-
+  }
 `;
 
-export const SubmitButton = styled.button.attrs({
-  type: 'submit'
-})`
-background: #0D2636;
-border: 0;
-border-radius: 4px;
-margin-left: 10px;
-padding: 0 15px;
-display: flex;
-justify-content: center;
-align-items: center;
+//Criando animação do botão
+const animate = keyframes`
+from{
+  transform: rotate(0deg);
+}
+
+to{
+  transform: rotate(360deg);
+}
+`;
+
+export const SubmitButton = styled.button.attrs((props) => ({
+  type: "submit",
+  disabled: props.loading,
+}))`
+  background: #0d2636;
+  border: 0;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+
+  ${(props) =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${animate} 2s linear infinite;
+      }
+    `}
 `;
